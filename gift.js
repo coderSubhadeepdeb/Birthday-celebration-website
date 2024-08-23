9bba51a717cb9a68ab5c3333c00bd2c88561c42e
coderSubhadeepdeb <sbhdpdeb@gmail.com>
2024-08-23 21:11:03 +0530

    new files added

---
+++ gift.js
@@ -0,0 +1,50 @@
+// script.js
+
+// Get the card element
+const cardone = document.getElementById("flip-card-one");
+const cardtwo = document.getElementById("flip-card-two");
+const cardthree = document.getElementById("flip-card-three");
+const cardfour = document.getElementById("flip-card-four");
+const cardfive = document.getElementById("flip-card-five");
+const cardsix = document.getElementById("flip-card-six");
+const cardseven = document.getElementById("flip-card-seven");
+const cardeight = document.getElementById("flip-card-eight");
+const cardnine = document.getElementById("flip-card-nine");
+
+// Add click event listener to the card
+cardone.addEventListener("click", function() {
+    // Toggle the flip class on click
+    cardone.classList.toggle("flipped");
+});
+cardtwo.addEventListener("click", function() {
+    // Toggle the flip class on click
+    cardtwo.classList.toggle("flipped");
+});
+cardthree.addEventListener("click", function() {
+    // Toggle the flip class on click
+    cardthree.classList.toggle("flipped");
+});
+cardfour.addEventListener("click", function() {
+    // Toggle the flip class on click
+    cardfour.classList.toggle("flipped");
+});
+cardfive.addEventListener("click", function() {
+    // Toggle the flip class on click
+    cardfive.classList.toggle("flipped");
+});
+cardsix.addEventListener("click", function() {
+    // Toggle the flip class on click
+    cardsix.classList.toggle("flipped");
+});
+cardseven.addEventListener("click", function() {
+    // Toggle the flip class on click
+    cardseven.classList.toggle("flipped");
+});
+cardeight.addEventListener("click", function() {
+    // Toggle the flip class on click
+    cardeight.classList.toggle("flipped");
+});
+cardnine.addEventListener("click", function() {
+    // Toggle the flip class on click
+    cardnine.classList.toggle("flipped");
+});
